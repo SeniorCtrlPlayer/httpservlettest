917e2a35426b38a52bf85a48b2dc985c06f68092
SeniorCtrlPlayer <510062390@gmail.com>
2018-12-08 11:23:01 +0800

    访问localhost:8080/httpservlet,servlet加载,control成功请求数

--- src/com/lwk/servlet/myhttpservlet.java
+++ src/com/lwk/servlet/myhttpservlet.java
@@ -7,13 +7,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class myhttpservlet extends HttpServlet {
+    private int Getn=0;
+    private int Postn=0;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("第"+(++Getn)+"个get请求");
         super.doGet(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("第"+(++Postn)+"个post请求");
         super.doPost(req, resp);
     }
 }
