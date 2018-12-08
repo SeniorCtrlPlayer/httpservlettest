e4c220f1e33387981020a8f070a7da538e5b577e
SeniorCtrlPlayer <510062390@gmail.com>
2018-12-08 20:52:20 +0800

    用户Dao接口

---
+++ src/com/lwk/dao/UserinfoDao.java
@@ -0,0 +1,59 @@
+package com.lwk.dao;
+
+import com.lwk.exception.IdIsNullException;
+import com.lwk.model.Userinfo;
+
+import java.util.List;
+
+public interface UserinfoDao {
+
+    /**
+     * 根据用户名查询单个用户
+     *
+     * @param name
+     * @return
+     */
+    public Userinfo findUserByName(String name);
+
+    /**
+     * 根据id查询单个用户
+     *
+     * @param id
+     * @return
+     * @throws IdIsNullException
+     */
+    public Userinfo findUserById(Integer id)throws IdIsNullException;
+
+    /**
+     * 查询所有用户
+     *
+     * @return
+     */
+    public List<Userinfo> findAllUsers();
+
+    /**
+     * 添加用户
+     *
+     * @param userinfo
+     * @return
+     */
+    public int addUser(Userinfo userinfo);
+
+    /**
+     * 根据id删除单个用户
+     *
+     * @param id
+     * @return
+     * @throws IdIsNullException
+     */
+    public int deleteUserById(Integer id)throws IdIsNullException;
+
+    /**
+     * 根据id修改单个用户
+     *
+     * @param userinfo
+     * @return
+     * @throws IdIsNullException
+     */
+    public int updateUserById(Userinfo userinfo)throws IdIsNullException;
+}
