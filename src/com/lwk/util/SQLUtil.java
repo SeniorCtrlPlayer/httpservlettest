ebf2f9f9334801d921c998943b68b7bbbafe2312
SeniorCtrlPlayer <510062390@gmail.com>
2018-12-11 11:13:24 +0800

    在SQLUtil部分将rs直接转换成arraylist，关闭conn之后直接返回arraylist

--- src/com/lwk/util/SQLUtil.java
+++ src/com/lwk/util/SQLUtil.java
@@ -1,8 +1,7 @@
 package com.lwk.util;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class SQLUtil {
 
@@ -10,6 +9,13 @@ public class SQLUtil {
     private static PreparedStatement ps = null;
     private static ResultSet rs = null;
 
+    public static Connection getConn() {
+        return conn;
+    }
+
+    public static PreparedStatement getPs() {
+        return ps;
+    }
 
     public static ResultSet executeQuery(String sql, String[] params) {
         try {
@@ -30,10 +36,59 @@ public class SQLUtil {
         }catch (Exception e){
             e.printStackTrace();
         }finally {
-            JdbcUtil.release(null, ps, conn);
+//            JdbcUtil.release(null, ps, conn);延迟关闭
             System.out.println("数据库连接已释放");
             return rs;
 //            返回的rs无法使用
+        }
+    }
+
+    //executeQueryRightDown
+    public static ArrayList<Object[]> executeQueryRD(String sql, Object params[]) {
+
+        ArrayList<Object[]> arrayList=null;
+
+
+        try {
+            conn = JdbcUtil.getConnection();
+            ps = conn.prepareStatement(sql);
+
+            // 给?赋值
+            if (params != null && params.length > 0) {
+
+                for (int i = 0; i < params.length; i++) {
+                    ps.setObject(i + 1, params[i]);
+                }
+            }
+            rs = ps.executeQuery();
+
+            //封装arrayList
+
+            ResultSetMetaData rsmd=rs.getMetaData();//元数据
+            int columnCount=rsmd.getColumnCount();
+            System.out.println("columnCount is: "+columnCount);
+
+            arrayList=new ArrayList<Object[]>();
+
+            while(rs.next()){
+                //封装Object[]
+                Object[] objects=new Object[columnCount];
+                for (int i = 0; i < columnCount; i++) {
+                    objects[i]=rs.getObject(i+1);
+                    //测试语句
+//                    System.out.println(objects[i]);
+                }
+
+                arrayList.add(objects);
+
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            JdbcUtil.release(rs, ps, conn);//立即关闭
+            return arrayList;
         }
     }
 }
--- src/com/lwk/util/SQLUtiltest.java
+++ src/com/lwk/util/SQLUtiltest.java
@@ -4,6 +4,7 @@ import com.lwk.exception.DaoException;
 import com.lwk.model.Userinfo;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class SQLUtiltest {
     public static void main(String[] args) {
@@ -13,7 +14,7 @@ public class SQLUtiltest {
             String[] params = { "123","123" };
 
             //dao中不在含有数据库连接部分
-            ResultSet rs = SQLUtil.executeQuery(sql, params);
+//            ResultSet rs = SQLUtil.executeQuery(sql, params);
 //            if(rs!=null){
 //                System.out.println("结果集不为空，但是conn已被关闭，rs无法使用");
 //            }
@@ -21,6 +22,12 @@ public class SQLUtiltest {
 //                System.out.println(rs.getString("name"));
 //                System.out.println(rs.getString("pwd"));
 //            }
+            ArrayList<Object[]> al = SQLUtil.executeQueryRD(sql,params);
+            if(al != null && al.size() > 0){
+                Object[] objects = al.get(0);
+                System.out.println((String)objects[0]);
+                System.out.println((String)objects[1]);
+            }
         }catch (Exception e){
             //抛出Dao的运行时异常
             e.printStackTrace();
